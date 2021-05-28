e161ebe8117b38a41ace4de53fae4d1a241a947e
Ilona Shevchuk <ilona.shvk@icloud.com>
2021-05-28 22:41:34 +0300

    scss8.3

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,12 @@
-;() => {
+;(() => {
   const menuBtnRef = document.querySelector('[data-menu-button]')
   const mobileMenuRef = document.querySelector('[data-menu]')
-  const mobileBtnClose = document.querySelector('[data-menu-close]')
   menuBtnRef.addEventListener('click', () => {
+    console.log(' menuBtnRef', menuBtnRef)
+    console.log(' mobileMenuRef', mobileMenuRef)
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false
+    menuBtnRef.classList.toggle('is-open')
+    menuBtnRef.setAttribute('aria-expanded', !expanded)
     mobileMenuRef.classList.toggle('is-open')
   })
-  mobileBtnClose.addEventListener('click', () => {
-    mobileMenuRef.classList.toggle('is-open')
-  })
-}
+})()
